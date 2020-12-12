4a3772288c119c670806280e177f382c1c4ccdef
user9747 <padinju@gmail.com>
2020-12-12 21:33:44 +0530

    Updates

---
+++ precache-manifest.7025f9ac902e3c14218c361ac73dc631.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8caf4437e7545c47ae9f43d98c213474",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d7a995875d0d96bb8727",
+    "url": "./static/css/main.fb6823d6.chunk.css"
+  },
+  {
+    "revision": "cc57e27b72fc1696af8d",
+    "url": "./static/js/2.a50fad24.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.a50fad24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7a995875d0d96bb8727",
+    "url": "./static/js/main.c2ae2aba.chunk.js"
+  },
+  {
+    "revision": "0cd8abe25ac1f70f0c13",
+    "url": "./static/js/runtime-main.5162fda1.js"
+  }
+]);
